bbe054e03aff33fc37ddf55c68fb79c03cc3430b
tefedrb-enterprise <tefebell@gmail.com>
2019-10-09 15:23:05 -0400

    setup post model/repo/services

--- backend/src/main/java/project2/backend/Services/PostService.java
+++ backend/src/main/java/project2/backend/Services/PostService.java
@@ -4,6 +4,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import project2.backend.Models.Post;
 
 public interface PostService {
-    Post createPost(Post newPost);
-    Post deletePost(Post post);
+    public Post createPost(Post newPost);
+    public void deletePostById(Long postId);
+    public Iterable<Post> listAllPosts();
 }
